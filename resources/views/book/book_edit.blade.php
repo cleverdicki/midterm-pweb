dc59feed04013b0435e741125a4e9c20f759e432
FXKevinK <kevinkoes.fx@gmail.com>
2020-10-31 19:15:27 +0700

    Done (Transaction)

--- resources/views/book/book_edit.blade.php
+++ resources/views/book/book_edit.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <h1 class="mt-3">Form edit data buku</h1>
+            <h1 class="mt-3">Edit Book Data Form</h1>
             <form method="POST" action="{{ url('/listofbook/updatebook/'.$dt->id)}}">
                 @method('patch')
                 @csrf
@@ -15,31 +15,31 @@
                 </div>
                 <div class="form-group">
                     <label for="isbn">ISBN</label>
-                    <input type="text" class="form-control" id="isbn" placeholder="Masukkan ISBN" name="ISBN" value="{{ $dt->ISBN }}">
+                    <input type="text" class="form-control" id="isbn" placeholder="Insert ISBN" name="ISBN" value="{{ $dt->ISBN }}">
                 </div>
                 <div class="form-group">
                     <label for="genre_book">Book Genre</label>
-                    <input type="text" class="form-control" id="genre_book" placeholder="Masukkan genre buku" name="genre" value="{{ $dt->genre }}">
+                    <input type="text" class="form-control" id="genre_book" placeholder="What is the genre of the book?" name="genre" value="{{ $dt->genre }}">
                 </div>
                 <div class="form-group">
                     <label for="quantity_book">Book Quantities</label>
-                    <input type="text" class="form-control" id="quantity_book" placeholder="Masukkan jumlah buku" name="stock" value="{{ $dt->stock }}">
+                    <input type="text" class="form-control" id="quantity_book" placeholder="Insert book stock" name="stock" value="{{ $dt->stock }}">
                 </div>
                 <div class="form-group">
                     <label for="writer_book">Author</label>
-                    <input type="text" class="form-control" id="writer_book" placeholder="Masukkan penulis buku" name="writer" value="{{ $dt->writer }}">
+                    <input type="text" class="form-control" id="writer_book" placeholder="Who is the author?" name="writer" value="{{ $dt->writer }}">
                 </div>
                 <div class="form-group">
                     <label for="publisher_book">Publisher</label>
-                    <input type="text" class="form-control" id="publisher_book" placeholder="Masukkan penerbit buku" name="publisher" value="{{ $dt->publisher }}">
+                    <input type="text" class="form-control" id="publisher_book" placeholder="Who is the publisher?" name="publisher" value="{{ $dt->publisher }}">
                 </div>
                 <div class="form-group">
                     <label for="year_book">Year of Issue</label>
-                    <input type="text" class="form-control" id="year_book" placeholder="Masukkan tahun terbit buku" name="year" value="{{ $dt->year }}">
+                    <input type="text" class="form-control" id="year_book" placeholder="Year of publication" name="year" value="{{ $dt->year }}">
                 </div>
                 <div class="form-group">
                     <label for="desc_book">Book Description</label>
-                    <input type="text" class="form-control" id="desc_book" placeholder="Masukkan deskripsi buku" name="desc" value="{{ $dt->desc }}">
+                    <input type="text" class="form-control" id="desc_book" placeholder="Any description for this book?" name="desc" value="{{ $dt->desc }}">
                 </div>
                 <button type="submit" class="btn btn-primary mb-3">Edit data</button>
               </form>
